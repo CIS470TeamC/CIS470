9d2407af5001540e37db292e31373f70159a2243
AndyCIS470 <andy.kalman@hotmail.com>
2014-10-11 18:20:31 -0700

    DataLayer
    Creating connections and changing folder structure for best practice

---
+++ App_Code/DataLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//adding using statements to manage datasources
+using System.Data.OleDb;
+using System.Net;
+using System.Data;
+
+/// <summary>
+/// Summary description for DataLayer
+/// </summary>
+public class DataLayer
+{
+
+
+
+	public DataLayer()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
