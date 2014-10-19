d8ca6c5078fae09cc6bb988f7ed92e7bbd5679be
AndyCIS470 <andy.kalman@hotmail.com>
2014-10-18 21:44:12 -0700

    Working grid view
    Need to edit Grid to reflect meaningful columns

--- frmUpdateOrder.aspx.cs
+++ frmUpdateOrder.aspx.cs
@@ -7,16 +7,19 @@ using System.Web.UI.WebControls;
 using System.Web.Security;
 using System.Configuration;
 using System.Web.SessionState;
+using System.Data.OleDb;
 
 public partial class frmUpdateOrder : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
         if (Session["SecurityLevel"] == null)
         {
             Response.Redirect("frmLogin.aspx");
         }
     }
+
     protected void lbtnLogout_Click(object sender, EventArgs e)
     {
 
